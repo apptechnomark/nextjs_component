fb15e36a22161caa950ca0691839c848bd7ac6fa
Mihir Savalia <mihir.savalia@technomark.io>
2023-10-12 12:09:42 +0530

    CommentSentBox,Tab,NavigationBar,RangeSlider

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -23,23 +23,25 @@ const TabBar = ({ tabs, getValue, className, alignIcon }: TabBarProps) => {
   };
 
   return (
-    <div className={`h-12 flex ${style.customScrollbar}  ${className} overflow-x-auto`}>
+    <div className={`h-12 flex ${style.customScrollbar}  ${className} overflow-visible`}>
       {tabs.map((tab, index: any) => (
         <div
           key={index + 1}
           id={tab.id}
-          className={` max-[320px]:!min-w-full w-full relative flex !m-0 !p-0 select-none ${tab.disabled == true ? "text-slatyGrey hover:pointer-events-none" : "cursor-pointer hover:text-primary hover:border-b-2 hover:border-primary hover:bg-[#E1F7F3]"} justify-center items-center border-b-2 border-lightSilver text-center  ${tabId === tab.id ? "border-b-2 border-primary text-primary" : ""}`}
-          onClick={tab.disabled ? null : handleClick}
+          className={`max-[320px]:!min-w-full w-full relative flex select-none ${tab.disabled == true ? "text-slatyGrey hover:pointer-events-none" : "cursor-pointer hover:text-primary hover:border-b-2 hover:border-primary hover:bg-[#E1F7F3]"} ${tab.jsxElement ? "!m-0 !p-0" : "px-5"} justify-center items-center border-b-2  text-center  ${tabId === tab.id ? " border-primary text-primary" : "border-lightSilver"}`}
+          onClick={handleClick}
         >
-          {tab.alignIcon == "left" && <>{tab.icon}&nbsp;</>}
-          {tab.label}&nbsp;
-          {tab.alignIcon == "right" && <>{tab.icon}</>}
-          {tab.jsxElement}
+          {tab.alignIcon === "left" && tab.icon}
+          {tab.label}
+          {tab.alignIcon === "right" && tab.icon}
+          <span className="mx-0.5">
+            {tab.jsxElement}
+          </span>
           <span className={` absolute ${isClicked && tabId === tab.id && `${style.tab__ripple} w-full opacity-0 h-12 rounded-[30px]`}`}></span>
         </div>
       ))
       }
-    </div >
+    </div>
   );
 };
 
