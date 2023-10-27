2e53c4941da7cf1c29079e8b969cf7440c3cc680
Alvish6654 <alvish.pipaliya@technomark.io>
2023-10-27 17:26:29 +0530

    Datatable changes

--- src/components/Datatable/Datatable.tsx
+++ src/components/Datatable/Datatable.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import ChevronRight from "./icons/ChevronRight";
 import SortIcon from "./icons/SortIcon";
+import styles from "./Datatable.module.scss";
 
 interface AComponentProps {
   children: string;
@@ -50,7 +51,6 @@ const DataTable = ({
   const tableRef = useRef<HTMLTableElement>(null);
   const [sortConfig, setSortConfig] = useState({ key: null, direction: "" });
   const [expandedRows, setExpandedRows] = useState(new Set());
-  const [columnStyle, setColumnStyle] = useState([])
 
   const handleSort = (columnKey: any) => {
     let direction = "asc";
@@ -133,26 +133,32 @@ const DataTable = ({
   };
 
   return (
-    <div className={`h-full`}>
-      <table className="w-full">
-        <thead>
+    <div className={`h-full` }>
+      <table className={`w-full `}>
+        <thead className={`${sticky && styles.customDataTable} `}>
           <tr
-            className={`w-full top-0 z-[5]  ${sticky ? "sticky bg-pureWhite " : "static border-y border-pureBlack"
-              } ${noHeader ? "hidden " : ""}`}
+            className={`top-0 z-[5]  ${
+              sticky
+                ? `shadow-md  sticky bg-[#f2f2f2]`
+                : "static border-y border-pureBlack"
+            } ${noHeader ? "hidden" : ""}`}
           >
             {expandable && (
-              <th className={`w-8 ${expandableStyle?.columns}`}></th>
+              <th className={`w-5 ${expandableStyle?.columns}`}></th>
             )}
             {columns?.map((column, colIndex) => (
               <th
-                className={`${column.colStyle} p-2 font-proxima h-12 text-sm font-bold whitespace-nowrap ${column.sortable ? "cursor-pointer" : "cursor-default"
-                  }`}
+                className={`${
+                  column.colStyle
+                } h-12 text-sm font-bold p-2 whitespace-nowrap ${
+                  column.sortable ? "cursor-pointer" : "cursor-default"
+                }`}
                 key={colIndex}
                 onClick={() => column.sortable && handleSort(column.accessor)}
               >
                 {column.sortable ? (
                   <span
-                    className={`flex items-center font-proxima justify-${getAlignment(
+                    className={`flex items-center justify-${getAlignment(
                       column.colalign
                     )} gap-2`}
                   >
@@ -166,7 +172,7 @@ const DataTable = ({
                   </span>
                 ) : (
                   <span
-                    className={`flex font-proxima items-center justify-${getAlignment(
+                    className={`flex items-center justify-${getAlignment(
                       column.colalign
                     )}`}
                   >
@@ -184,25 +190,31 @@ const DataTable = ({
                 {expandable &&
                   (row.details ? (
                     <td
-                      className={`${expandableStyle?.rows} h-12 ${expandedRows.has(rowIndex) ? "border-none" : "border-b"}  border-[#ccc] cursor-pointer`}
+                      className={`${expandableStyle?.rows} h-12 pl-2  border-b border-[#ccc] cursor-pointer`}
                       onClick={() => handleRowToggle(rowIndex)}
                     >
-                      <div className={`flex justify-center items-center transition-transform ${expandedRows.has(rowIndex) || isExpanded ? "rotate-90 duration-300" : "duration-200"}`}>
+                      <div
+                        className={`flex items-center justify-center transition-transform ${
+                          expandedRows.has(rowIndex) || isExpanded
+                            ? "rotate-90 duration-300"
+                            : "duration-200"
+                        }`}
+                      >
                         <ChevronRight />
                       </div>
                     </td>
                   ) : (
                     <td
-                      className={`w-8 ${expandableStyle?.rows} h-12 pl-2 font-proxima ${expandedRows.has(rowIndex) ? "border-none" : "border-b"} ${noHeader && "border-t"} border-[#ccc] cursor-pointer`}
+                      className={`${expandableStyle?.rows} h-12 pl-2  border-b border-[#ccc] cursor-pointer`}
                     ></td>
                   ))}
                 {columns?.map((column, colIndex) => (
                   <td
                     key={colIndex}
-                    className={` ${row?.style} ${noHeader && column.colStyle} ${column.rowStyle} h-12 text-xs py-2 px-1 ${expandedRows.has(rowIndex) ? "border-none" : "border-b"} border-[#ccc] break-all ${noHeader && "border-t"}`}
+                    className={`${row?.style} ${column.rowStyle} h-12 text-xs p-2 border-b border-[#ccc] break-all`}
                   >
                     <span
-                      className={`flex py-2 px-1 font-proxima items-center justify-${getAlignment(
+                      className={`flex items-center justify-${getAlignment(
                         column.colalign
                       )}`}
                     >
@@ -213,11 +225,11 @@ const DataTable = ({
               </tr>
               {(expandedRows.has(rowIndex) || isExpanded) && (
                 <tr>
-                  <td className="font-proxima" colSpan={columns.length + 1}>
+                  <td colSpan={columns.length + 1}>
                     {row.details ? (
                       row.details
                     ) : (
-                      <div className={`m-3 font-proxima ${expandableStyle?.rows}`}>
+                      <div className={`m-3 ${expandableStyle?.rows}`}>
                         No data to display
                       </div>
                     )}
@@ -228,7 +240,7 @@ const DataTable = ({
           ))}
         </tbody>
       </table>
-    </div >
+    </div>
   );
 };
 
